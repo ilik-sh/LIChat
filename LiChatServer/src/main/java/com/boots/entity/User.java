115401f6a740412a8551a4cab8b17da2acb608a5
4Siris <leonid_voynov@mail.ru>
2023-07-16 15:23:29 +0300

    Update hibernate version and hibernate's methods of using Add Spring security Signed-off-by: 4Siris <leonid_voynov@mail.ru>

---
+++ LiChatServer/src/main/java/com/boots/entity/User.java
@@ -0,0 +1,76 @@
+package com.boots.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "account")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Size(min=2, message = "Не меньше 5 знаков")
+    @Getter
+    @Setter
+    private String username;
+
+    @Size(min=2, message = "Не меньше 5 знаков")
+    @Getter
+    @Setter
+    private String password;
+
+    @Transient
+    @Getter
+    @Setter
+    private String passwordConfirm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @Getter
+    @Setter
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String passwordConfirm) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
