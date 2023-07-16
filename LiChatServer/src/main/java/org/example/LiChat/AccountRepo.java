9be2e8ed26bbdc5892a265ec6a0eeb5bd9ec3189
4Siris <leonid_voynov@mail.ru>
2023-07-16 15:25:34 +0300

    Clear unnecessary files Signed-off-by: 4Siris <leonid_voynov@mail.ru>

--- LiChatServer/src/main/java/org/example/LiChat/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package org.example.LiChat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<Account,Long> {
-    Account findByUsername(String username);
-}
