ba441fea3e55ce768301eb9db5a214eb3cd4af83
4Siris <leonid_voynov@mail.ru>
2023-08-27 16:31:09 +0300

    Clear code Signed-off-by: 4Siris <leonid_voynov@mail.ru>

--- LiChatServer/src/main/java/com/boots/controller/AdminController.java
+++ LiChatServer/src/main/java/com/boots/controller/AdminController.java
@@ -2,7 +2,6 @@ package com.boots.controller;
 
 import com.boots.entity.User;
 import com.boots.service.UserService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
